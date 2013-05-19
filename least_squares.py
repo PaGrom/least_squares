91331287445d1fc2ed895f7f38b44e28f027bc4c
PaGrom <pagrom23@gmail.com>
2013-05-19 16:19:32 +0400

    Ended 3.1.1

--- least_squares.py
+++ least_squares.py
@@ -6,6 +6,10 @@ from pylab import * # for plotting commands
 import numpy as np
 import random
 
+# 3. Практическая часть
+# 3.1. Исследование для первого воздействия
+# 3.1.1. Построение имитатора объекта и ИИС
+
 
 # Установим объем выборки, предъявляемой к измерению
 N = 1000
@@ -28,7 +32,7 @@ def deriv(Y, t):
 yinit = array([1, 1])
 
 # Временные границы эксперимента
-time = linspace(0.0, 10.0, N)
+time = linspace(0.0, 100.0, N)
 
 # Решаем получившееся ДУ
 y = odeint(deriv, yinit, time)
@@ -53,16 +57,16 @@ n = np.random.normal(0, 0.01, N)
 # Вектор измерений
 Z = [y1[i] + n[i] for i in range(N)]
 
-# Построим график имитатора объекта при наличии случайных возмущений на интервале 0..10 с delta = 0.01 с
+# Построим график имитатора объекта при наличии случайных возмущений на интервале 0..100 с delta = 0.01 с
 fig.clear()
 plot(time, Z)
 xlabel('t')
 ylabel('Z')
 savefig("f2.png")
 
-# Построим график имитатора объекта при наличии случайных возмущений на интервале 0..1 с delta = 0.01 с
+# Построим график имитатора объекта при наличии случайных возмущений на интервале 0..10 с delta = 0.01 с
 fig.clear()
-time = linspace(0.0, 1.0, 100)
+time = linspace(0.0, 10.0, 100)
 subplot(111)
 plot(time, y1[:100], label="y1")
 plot(time, Z[:100], label="Z")
@@ -70,3 +74,8 @@ legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
 		ncol=2, mode="expand", borderaxespad=0.)
 xlabel('t')
 savefig("f3.png")
+
+# Этап построение иммитатора объекта заканчивается на получении вектора измерений
+print "y1:\t\t\tn:\t\t\t\tZ:"
+for i in xrange(15):
+	print y1[i], "\t\t", n[i], "\t\t", Z[i]
