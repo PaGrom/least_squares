7206677dda5ed5a8bf8110420eeba89c7132c609
PaGrom <pagrom23@gmail.com>
2013-05-19 15:32:26 +0400

    Third commit

--- least_squares.py
+++ least_squares.py
@@ -52,10 +52,20 @@ n = [random.uniform(0, 0.01) for i in range(N)]
 # Вектор измерений
 Z = [y1[i] + n[i] for i in range(N)]
 
-# Построим график имитатора объекта при наличии случайных возмущений на интервале 0..10 с = 0.01 с
+# Построим график имитатора объекта при наличии случайных возмущений на интервале 0..10 с delta = 0.01 с
 fig.clear()
-subplot(111)
 plot(time, Z)
 xlabel('t')
-xlabel('Z')
+ylabel('Z')
 savefig("f2.png")
+
+# Построим график имитатора объекта при наличии случайных возмущений на интервале 0..1 с delta = 0.01 с
+fig.clear()
+time = linspace(0.0, 1.0, 100)
+subplot(111)
+plot(time, y1[:100], label="y1")
+plot(time, Z[:100], label="Z")
+legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+		ncol=2, mode="expand", borderaxespad=0.)
+xlabel('t')
+savefig("f3.png")
