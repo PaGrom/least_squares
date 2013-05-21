a31717b6376c139564ca10bc53241b31dda43a5e
PaGrom <pagrom23@gmail.com>
2013-05-21 21:30:53 +0400

    Нашел собственные значения и собственные вектора

--- least_squares.py
+++ least_squares.py
@@ -48,7 +48,7 @@ plot(time, y2, label="y2")
 legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
 		ncol=2, mode="expand", borderaxespad=0.)
 xlabel('t')
-savefig("f1.png")
+savefig("1.1.png")
 
 # Построим имитатора объекта
 
@@ -63,7 +63,7 @@ fig.clear()
 plot(time, Z)
 xlabel('t')
 ylabel('Z')
-savefig("f2.png")
+savefig("1.2.png")
 
 # Построим график имитатора объекта при наличии случайных возмущений на интервале 0..10 с delta = 0.01 с
 fig.clear()
@@ -74,11 +74,11 @@ plot(time, Z[:N / 50], label="Z")
 legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
 		ncol=2, mode="expand", borderaxespad=0.)
 xlabel('t')
-savefig("f3.png")
+savefig("1.3.png")
 
 # Этап построение иммитатора объекта заканчивается на получении вектора измерений
 print "y1:\t\t\tn:\t\t\t\tZ:"
-for i in xrange(15):
+for i in xrange(N):
 	print y1[i], "\t\t", n[i], "\t\t", Z[i]
 
 
@@ -137,6 +137,7 @@ C2 = np.array([U[:, 3]])
 
 # Получим матрицу измерений:
 C = np.column_stack((C1.T, C2.T))
+print C
 
 # Построим график чувствительности dy/d*teta1
 fig.clear()
@@ -144,7 +145,7 @@ time = linspace(0.0, 100.0, N)
 plot(time, C[:, 0][:])
 xlabel('t')
 ylabel('C1')
-savefig("f4.png")
+savefig("1.4.png")
 
 # Построим график чувствительности dy/d*teta2
 fig.clear()
@@ -152,19 +153,90 @@ time = linspace(0.0, 100.0, N)
 plot(time, C[:, 1][:])
 xlabel('t')
 ylabel('C2')
-savefig("f5.png")
+savefig("1.5.png")
 
 # 3.1.4. Реализация МНК-алгоритма
 
 # Определим требуемые оценки параметров
 # teta_mnk = (C.T * C)^-1 * C.T * Z
-Q = np.matrix(C.T.dot(C))
-print Q
+Q = np.matrix(C.T.dot(C)) # C.T * C
+A = Q.I # (C.T * C)^-1 
+print A
+# [  3.11617423e-06  -1.57025125e-08]
+# [ -1.57025125e-08   1.60457589e-08]
+print "det: ", 3.11617423e-06*1.60457589e-08 - 1.57025125e-08*1.57025125e-08
 
 b = C.T.dot(Z)
 print b
 
-teta_mnk = Q.I.dot(b)
+teta_mnk = A.dot(b)
 teta_mnk = np.array([abs(teta_mnk[:, i]) for i in range(2)])
 
 print teta_mnk
+
+# 3.1.5. Построение эллипса рассеяния
+
+# det(lambda*E - A)
+
+# [lambda - 3.11617423e-06           1.57025125e-08]
+# [         1.57025125e-08  lambda - 1.60457589e-08]
+
+# (lamda - 3.11617423e-06)*(lambda - 1.60457589e-08) - 1.57025125e-08 * 1.57025125e-08 =
+# = lambda^2 - 1.60457589e-08*lambda - 3.11617423e-06*lambda + 5.0001380384973146e-14 - 2.4656889881265625e-16=
+# = lambda^2 - 3.1322199889e-06*lambda + 4.975481148616049e-14
+
+#Подсчет и вывод корней уравнения    
+def equation_solve(a,b,c):
+    D=b**2-4*a*c
+    if a:
+        if D>0:
+            x1=(-b+D**0.5)/(2*a)
+            x2=(-b-D**0.5)/(2*a)
+            print "Корни уравнения:\n","x1 =",x1,"\nx2 =",x2
+            return [x1, x2]
+        if D==0:
+            x1=(-b)/(2*a)
+            print "Корень уравнения:\n","x1 = x2 =",x1
+        if D<0:
+            print "Корни уравнения:"
+            print 'x1 = '-b/2*a+math.sqrt(D)/2*abs(a)
+            print 'x2 = '-b/2*a-math.sqrt(D)/2*abs(a)
+    elif b:
+        x1=-c/b
+        print'Корень уравнения:\n','x =',x1
+    elif c:
+        print'Уравнение неверно'
+    else:
+        print'Уравнение верно'
+
+# Находим длины полуосей
+lambda1, lambda2 = equation_solve(1, - 3.1322199889e-06, 4.975481148616049e-14)
+a1 = math.sqrt(lambda1) # 0.00176529141023
+a2 = math.sqrt(lambda2) # 0.000126357531956
+
+# Находим напрвление полуосей
+
+# Для lambda1 = 3.11625376302e-06:
+# [3.11625376302e-06 - 3.11617423e-06                      1.57025125e-08] [f1]   [0]
+# [                    1.57025125e-08  3.11625376302e-06 - 1.60457589e-08] [f2] = [0]
+
+# [7.953302000012681e-11     1.57025125e-08] [f1]   [0]
+# [       1.57025125e-08  3.10020800412e-06] [f2] = [0]
+
+# f1 = 1
+# f2 = - 197.43387714907547
+
+# Для lambda2 = 1.59662258821e-08:
+# [1.59662258821e-08 - 3.11617423e-06                      1.57025125e-08] [f1]   [0]
+# [                    1.57025125e-08  1.59662258821e-08 - 1.60457589e-08] [f2] = [0]
+
+# [-3.1002080041179e-06          1.57025125e-08] [f1]   [0]
+# [      1.57025125e-08  -7.953301790000126e-11] [f2] = [0]
+
+# f1 = 1
+# f2 = 0.005064986759284622
+
+# Строим эллипс рассеяния
+
+
+
